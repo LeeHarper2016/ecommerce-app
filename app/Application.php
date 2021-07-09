41ffc1ef3cc9b87fce478cc29ac006b957f6d1ef
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-09 04:06:37 -0500

    Update Application.php
    Updated: Application.php
    -> Added the Dotenv package, which will load env variables into the $_ENV global.
    -> Added usage of the DB class, which will be initiated upon startup.

--- app/Application.php
+++ app/Application.php
@@ -4,11 +4,18 @@ namespace App;
 
 use App\Http\Requests\Request;
 use App\Http\Routing\Router;
+use App\Database\DB;
+use Dotenv\Dotenv;
 
 require_once('Http/Routing/web.php');
 
 class Application {
     public function run() {
+        $env = Dotenv::createImmutable('../');
+        $env->load();
+
+        DB::initiate();
+
         $request = new Request();
 
         Router::resolve($request);
