15b565f1418d81c2d5ecb10158b0a0ad4fa0ed3f
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-11 06:30:13 -0500

    Update App.vue
    Updated: App.vue
    -> Implemented the NavbarMenu component.

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,13 +1,16 @@
 <template>
+  <NavbarMenu></NavbarMenu>
 </template>
 <style>
 
 </style>
 <script>
+import NavbarMenu from "./components/NavbarMenu.vue";
+
 export default {
   name: 'App',
   components: {
-
+    NavbarMenu
   },
   data() {
     return [
