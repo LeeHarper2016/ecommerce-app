108f0d4793c9c27faf477fee33b42503065818fa
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-11 06:29:53 -0500

    Create NavbarMenu.vue
    Created: NavbarMenu.vue
    -> This file will hold the navbar element.

---
+++ src/js/components/NavbarMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <a class="nav-link" href="/">Home</a>
+    <a class="nav-link" href="/products">Products</a>
+    <a class="nav-link" href="/about">About Us</a>
+    <a class="nav-link" href="/contact">Contact Us</a>
+    <a class="nav-link" href="/login">Login</a>
+  </nav>
+</template>
+<style>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100vw;
+  height: 5rem;
+  border-bottom: 2px solid black;
+}
+.nav-link {
+  color: black;
+  text-decoration: none;
+  font-size: 24px;
+  font-weight: bold;
+  transition: all 0.35s;
+}
+.nav-link:hover {
+  color: darkgray;
+}
+</style>
+<script>
+  export default {
+    name: 'NavbarMenu',
+  }
+</script>
