0ff928a91695215cde4514178ea650c5571e7a44
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-07-13 08:03:11 -0500

    Update NavbarMenu.vue
    Updated: NavbarMenu.vue
    -> Modified the Products anchor element to be a RouterLink component instead.
    -> As views are created, the rest of this navbar shall be updated.

--- src/js/components/NavbarMenu.vue
+++ src/js/components/NavbarMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <a class="nav-link" href="/">Home</a>
-    <a class="nav-link" href="/products">Products</a>
+    <RouterLink class="nav-link" to="/products">Products</RouterLink>
     <a class="nav-link" href="/about">About Us</a>
     <a class="nav-link" href="/contact">Contact Us</a>
     <a class="nav-link" href="/login">Login</a>
