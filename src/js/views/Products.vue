6f66e77995331f88571576e1ddf1b61530b36078
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-07-13 07:57:47 -0500

    Create Products.vue
    Created: Products.vue
    -> This will contain the view that will be displayed at the '/products' route.

---
+++ src/js/views/Products.vue
@@ -0,0 +1,14 @@
+<template>
+  <ProductList></ProductList>
+</template>
+<style>
+
+</style>
+<script>
+import ProductList from "../components/ProductList.vue";
+export default {
+  components: {
+    ProductList
+  }
+}
+</script>
