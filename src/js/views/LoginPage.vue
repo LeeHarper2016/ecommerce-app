b24d4465b9d8ad1cdab86fa6aa916ccb91da2a23
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-07-15 10:36:49 -0500

    Create LoginPage.vue
    Created: LoginPage.vue
    -> This page will hold the login form.
    -> The submitForm() method will be responsible for sending formdata to the '/api/user/login' route.

---
+++ src/js/views/LoginPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <form @submit.prevent="submitForm"
+          ref="login_form">
+      <label for="email">
+        <input type="email"
+               name="email"
+               id="email">
+      </label>
+      <label for="password">
+        <input type="password"
+               name="password"
+               id="password">
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+
+import axios from "axios";
+
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    async submitForm() {
+      let form = this.$refs['login_form'];
+      let data = new FormData(form);
+
+      let res = await axios.post('/api/user/login', data);
+
+      console.log(res.data);
+    }
+  }
+}
+</script>
