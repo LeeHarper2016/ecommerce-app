7130ecdd42c33a7e3b2bc6bf5d0543a31525389e
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-07-17 10:47:32 -0500

    Update Router.js
    Updated: Router.js
    -> Implemented a beforeEach() method call, which will check for authentication guards before sending the user to each route.

--- src/js/Router.js
+++ src/js/Router.js
@@ -3,6 +3,7 @@ import { createWebHistory, createRouter } from 'vue-router';
 import Home from "./views/Home.vue";
 import LoginPage from './views/LoginPage.vue';
 import Products from './views/Products.vue';
+import axios from "axios";
 
 const routes = [
     {
@@ -25,6 +26,21 @@ const routes = [
 const Router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+// Checks routes for authentication guards.
+Router.beforeEach((to, from, next) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        axios.get('/api/auth')
+            .then(res => {
+                console.log(res.data);
+                if (res.data.isOwner !== false) {
+                    next();
+                }
+            })
+    } else {
+        next();
+    }
 });
 
 export default Router;
