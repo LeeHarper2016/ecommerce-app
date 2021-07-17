93d6c32f656e092aa67a6628ffce72d9f369d75c
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-07-17 14:07:55 -0500

    Create Admin.vue
    Created: Admin.vue
    -> This view will contain the content for the Admin Panel.

---
+++ src/js/views/Admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Admin Panel</h1>
+    <div>
+      <RouterLink class="admin_btn"
+                  to="/products/create">Create Product</RouterLink>
+    </div>
+  </div>
+</template>
+<style>
+.admin_btn {
+
+}
+</style>
+<script>
+  export default {
+    name: 'Admin'
+  }
+</script>
