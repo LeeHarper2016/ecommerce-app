aa779077ac2ede8d1376e2ce4b3df46473605680
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-07-17 14:08:43 -0500

    Create ProductsCreate.vue
    Created: ProductsCreate.vue
    -> This view will contain the form for adding a new product to the database.

---
+++ src/js/views/ProductsCreate.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <form @submit.prevent="submitForm"
+          ref="product_form">
+      <label for="name">
+        <input type="text"
+               name="name"
+               id="name">
+      </label>
+      <label for="description">
+        <textarea rows="40"
+                  cols="40"
+                  name="description"
+                  id="description"></textarea>
+      </label>
+      <label for="price">
+        <input type="number"
+               name="price"
+               id="price">
+      </label>
+      <label for="img">
+        <input type="text"
+               name="img"
+               id="img">
+      </label>
+      <button type="submit">Create Product</button>
+    </form>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+
+import axios from "axios";
+
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    async submitForm() {
+      let form = this.$refs['product_form'];
+      let data = new FormData(form);
+
+      let res = await axios.post('/api/products', data);
+    }
+  }
+}
+</script>
