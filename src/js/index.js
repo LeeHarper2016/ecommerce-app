4821a5e91cabc10e187df9e79d563fcc4c6389f4
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-07-17 07:34:24 -0500

    Update index.js
    Updated: index.js
    -> Implemented the Vuex store.
    -> This will handle the state management of the Vue components.
    -> This may be refactored into its own file at a later date.

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,23 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 import Router from './Router';
 
 require('normalize.css');
 
+const store = createStore({
+    state() {
+        return {
+            user: null
+        }
+    },
+    mutations: {
+        setUser(state, user) {
+            state.user = user;
+        }
+    }
+})
 createApp(App).use(Router)
+    .use(store)
     .mount('#app');
