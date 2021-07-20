209f1efe5dfd8ac70d9a6337b909fc932295c972
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-07-20 11:55:30 -0500

    Update ProductList.vue
    Updated: ProductList.vue
    -> Updated the styling of the product cards.
    -> Implemented the price and product image to the product card.

--- src/js/components/ProductList.vue
+++ src/js/components/ProductList.vue
@@ -15,9 +15,18 @@
            v-for="product of products"
            v-show="product.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1"
            :key="product.id">
+        <img :src="product.img">
         <h2> {{ product.name }} </h2>
+        <span>Price: {{ product.price }}</span>
+        <hr>
         <p v-if="product.description.length > 255">{{ product.description.substring(0, 255) }}...</p>
         <p v-else>{{ product.description }}</p>
+
+        <div class="btn_menu">
+          <button>Add to Cart</button>
+          <button>View</button>
+        </div>
+
       </div>
     </transition-group>
   </div>
@@ -47,13 +56,28 @@ input {
 .product-card {
   display: block;
   width: 250px;
-  height: 400px;
   margin: 1rem;
   padding: 1rem;
   text-align: center;
   border: 5px solid black;
   border-radius: 1rem;
   overflow: hidden;
+}
+
+.product-card img {
+  width: 200px;
+  height: 200px;
+  border-radius: 1.5rem;
+}
+
+.product-card h2 {
+  margin: 10px 0 0 0;
+}
+
+.btn_menu {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
 }
 </style>
 <script>
