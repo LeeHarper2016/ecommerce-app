0c3382889af1dfa195c330e3b030d304822378f6
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-08-05 05:22:17 -0500

    Update Admin.vue
    Updated: Admin.vue
    -> Implemented formatting for buttons of the admin panel.
    -> Updated the formatting of the page.

--- src/js/views/Admin.vue
+++ src/js/views/Admin.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Admin Panel</h1>
-    <div>
+    <hr>
+    <div class="container">
       <RouterLink class="admin_btn"
                   to="/products/create">Create Product</RouterLink>
     </div>
@@ -9,7 +10,12 @@
 </template>
 <style>
 .admin_btn {
-
+  background: lavender;
+  padding: 1rem;
+  border-radius: 25px;
+  border: 1px solid black;
+  text-decoration: none;
+  color: black;
 }
 </style>
 <script>
